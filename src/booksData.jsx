e1f361de5913ce5155ffbcd043e1c680d2dbf271
Jayanth <kolleparajayanth@gmail.com>
2025-03-25 17:11:41 +0530

    Completed the assignment

--- src/booksData.jsx
+++ src/booksData.jsx
@@ -3,30 +3,30 @@
 const books = [
   {
     id: 1,
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
+    title: "Captain Cool: The Story of MS Dhoni",
+    author: "Gaurav Joshi",
     description:
-      "A novel about the serious issues of rape and racial inequality, but it is also full of warmth and humor.",
+      "A gripping biography that chronicles the journey of Mahendra Singh Dhoni, the legendary cricketer who led India to numerous victories and became one of the most celebrated captains in the world of cricket.",
     coverImage:
-      "https://images-na.ssl-images-amazon.com/images/I/81OdwZ9qA9L.jpg",
+      "https://media.licdn.com/dms/image/v2/D5612AQGPKHG6Y7qaqA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1713964308467?e=2147483647&v=beta&t=IyEKu4a8tTjispgiwCgMTb0su3tFqT0w_3SppFTOjn4",
   },
   {
     id: 2,
-    title: "1984",
-    author: "George Orwell",
+    title: "Virat Kohli: Chasing Greatness",
+    author: "Suresh Menon",
     description:
-      "A dystopian social science fiction novel and cautionary tale about the dangers of totalitarianism.",
+      "An inspiring account of Virat Kohli's relentless pursuit of excellence in the world of cricket. From his early struggles to becoming one of the greatest batsmen of all time.",
     coverImage:
-      "https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL.jpg",
+      "https://iansportalimages.s3.amazonaws.com/thumbnails/RAJK6426S.jpg",
   },
   {
     id: 3,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
+    title: "Rohit Sharma: The Hitman’s Journey",
+    author: "Abhishek Dubey",
     description:
-      "A novel that critiques the disillusionment and moral decay of the period following WWI.",
+      "The incredible story of Rohit Sharma, famously known as 'The Hitman,' tracing his rise from humble beginnings to becoming one of the most explosive opening batsmen in cricket history.",
     coverImage:
-      "https://images-na.ssl-images-amazon.com/images/I/81af+MCATTL.jpg",
+      "https://i.pinimg.com/736x/59/4c/dd/594cdd054803833de876108608679d69.jpg",
   },
   // Add more books as needed
 ];
